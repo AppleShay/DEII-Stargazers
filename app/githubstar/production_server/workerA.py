241af437534ab39b0e0a3f26f9c5a3dbed2d53ed
Lu <ccchanluuu@gmail.com>
2025-05-27 14:45:12 +0200

    Update workerA.py

--- app/githubstar/production_server/workerA.py
+++ app/githubstar/production_server/workerA.py
@@ -4,7 +4,7 @@ import joblib
 
 # Celery configuration
 CELERY_BROKER_URL = 'amqp://rabbitmq:rabbitmq@rabbit:5672/'
-CELERY_RESULT_BACKEND = 'rpc://'
+CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
 
 # Initialize Celery
 celery = Celery('workerA', broker=CELERY_BROKER_URL, backend=CELERY_RESULT_BACKEND)
@@ -14,10 +14,8 @@ model = joblib.load('final_model.pkl')
 
 @celery.task
 def get_predictions(X_input):
-    """
-    Accepts a 2D list of feature values (X_input) and returns model predictions.
-    Example: X_input = [[5, 116, 74, ..., 30], [...], ...]
-    """
     X = np.array(X_input)
-    predictions = model.predict(X)
-    return predictions.tolist()
+    predictions=[]
+    for i in range(5):
+        predictions.append(model.predict([X[i]]))
+    return predictions
